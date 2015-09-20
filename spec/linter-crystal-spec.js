9c8e0d08f21e94b475f999f8c4358503f7adb7a1
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-20 14:51:11 -0600

    Test all other files.
    Closes #8

--- spec/linter-crystal-spec.js
+++ spec/linter-crystal-spec.js
@@ -14,9 +14,44 @@ describe('The Crystal provider for AtomLinter', () => {
       return atom.workspace.open(__dirname + '/files/incorrect_method_type.cr').then(editor => {
         return lint(editor).then(messages => {
           expect(messages.length).toEqual(1)
-          //expect(messages[0].file).toEqual(__dirname + '/files/incorrect_method_type.cr')
           expect(messages[0].type).toEqual("error")
           expect(messages[0].text).toEqual("no overload matches 'accepts_int' with types String\nOverloads are:\n - accepts_int(int : Int)")
+        })
+      })
+    })
+  })
+
+  it('finds an error in "relative_requirement.cr"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/relative_requirement.cr').then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(1)
+          expect(messages[0].type).toEqual("error")
+          expect(messages[0].text).toEqual("type must be String, not (String | Int32)")
+        })
+      })
+    })
+  })
+
+  it('finds an error in "syntax_error.cr"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/syntax_error.cr').then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(1)
+          expect(messages[0].type).toEqual("error")
+          expect(messages[0].text).toEqual("unexpected token: :")
+        })
+      })
+    })
+  })
+
+  it('finds an error in "variable_type_already_defined.cr"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/variable_type_already_defined.cr').then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(1)
+          expect(messages[0].type).toEqual("error")
+          expect(messages[0].text).toEqual("type must be String, not (String | Int32)")
         })
       })
     })
